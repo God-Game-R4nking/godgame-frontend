7b76ae6e5ba500d2727920e1629f60c6be640628
YoungJunRoh <shdudwns59@gmail.com>
2024-10-14 21:21:39 +0900

    webFix

--- src/hooks/WebSockethook.jsx
+++ src/hooks/WebSockethook.jsx
@@ -73,23 +73,34 @@ const useWebSocket = () => {
     }
   }, [websocketUrl]);
 
-  const sendMessage = useCallback((requestBody) => {
+  const sendMessage = useCallback(async (requestBody) => {
     if (socket && isConnected) {
       const message = {
-        memberId : requestBody.memberId,
-        gameRoomId : requestBody.gameRoomId,
-        type : requestBody.type,
-        nickName : requestBody.nickName,
-        content : requestBody.content
+        memberId: requestBody.memberId,
+        gameRoomId: requestBody.gameRoomId,
+        type: requestBody.type,
+        nickName: requestBody.nickName,
+        content: requestBody.content,
       };
-      socket.send(JSON.stringify(message));
+  
+      try {
+        socket.send(JSON.stringify(message));
+        // 여기에 추가적인 후속 처리를 할 수 있습니다.
+        console.log("Message sent successfully:", message);
+      } catch (error) {
+        console.error("Error sending message:", error);
+        // 에러 핸들링
+      }
     }
   }, [socket, isConnected]);
-
  
-  const sendDrawingData = useCallback((data) => {
+  const sendDrawingData = useCallback(async(data) => {
     if (socket && isConnected) {
+      try{
       socket.send(JSON.stringify(data));
+      }catch(error){
+        console.error(error);
+      }
     }
   }, [socket, isConnected]);
 
